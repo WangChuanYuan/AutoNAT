13a466914eed2f328cdc222f73de03846b617227
Han Qi <my@rhanqtl.com>
2020-12-25 10:29:23 +0800

    ,,,

--- backend/update_ip.py
+++ backend/update_ip.py
@@ -2,17 +2,10 @@ import argparse
 
 from core import update_host
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Update IP of PC')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Update IP of PC')
     parser.add_argument('ip', help='ip')
     parser.add_argument('mask', help='mask')
-    parser.add_argument('--gw', help='gateway')
+    parser.add_argument('--gateway', help='gateway')
     args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    args = parse_args()
     update_host(args.ip, args.mask, args.gw)
